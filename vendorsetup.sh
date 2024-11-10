d4ea18dd0a71090ea746bdb20dda3e9c60b1fde0
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-11-10 17:33:55 +0530

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-# Vendor
-git clone https://github.com/cranckv3/vendor_xaomi_water.git vendor/xiaomi/water
-
-# Vendor-ims
-git clone https://github.com/cranckv3/proprietary_vendor_xiaomi_ims-water.git  vendor/xiaomi/ims-water
-
-# Sepolicy
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
